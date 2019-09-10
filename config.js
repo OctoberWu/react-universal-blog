16463fd70bf82f6145f4747e6b0cc137b6e14e35
Shih-Chiang Wu <october.wu@gmail.com>
2019-09-10 16:06:47 +0800

    update babel from 6 to 7

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // config.js
 export default {
   site: {
-    title: 'React Universal Blog'
+    title: 'Secrete Garden of React'
   },
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'react-universal-blog',
+    slug: process.env.COSMIC_BUCKET || '8cd46fb0-d2ca-11e9-9ea8-2b9d842385af',
     media_url: 'https://cosmicjs.com/uploads',
-    read_key: process.env.COSMIC_READ_KEY || '',
-    write_key: process.env.COSMIC_WRITE_KEY || ''
+    read_key: process.env.COSMIC_READ_KEY || 'SFIUGVzhPtiv42U27qQ0ltH4acfGwO5Ts3QFRRIjafIGvBS5T0',
+    write_key: process.env.COSMIC_WRITE_KEY || 'sjMKGBbCPsHo6YxNgJGa5zojffswHg5R81fWVrlvl1pp9fbWj1'
   },
 }
